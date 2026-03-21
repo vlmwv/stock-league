f340b19f158bfdbec094749b19b6097bd7453b98
mwlee80 <mwlee80@smartscore.kr>
2026-03-21 23:50:55 +0900

    style: 디자인 시스템 및 글로벌 스타일 구축

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,7 @@ export default defineNuxtConfig({
     compatibilityVersion: 4
   },
   compatibilityDate: '2024-11-01',
-  css: ['~/assets/css/main.css'],
+  css: ['~/assets/css/index.css'],
   devtools: { enabled: true },
   modules: [
     '@nuxt/ui',
