66475add7877b9e78e145aeb8b03c96f0396b1a5
mwlee80 <mwlee80@smartscore.kr>
2026-04-01 23:28:08 +0900

    루트 오류

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -70,7 +70,7 @@ export default defineNuxtConfig({
       type: 'module'
     },
     workbox: {
-      navigateFallback: '/',
+      navigateFallback: '/index.html',
       globPatterns: ['**/*.{js,css,html,png,svg,ico}']
     }
   },
