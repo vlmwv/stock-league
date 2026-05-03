6a4e44861c499dbd03fcd95cfef0e7c1db934d18
mwlee80 <mwlee80@smartscore.kr>
2026-05-03 23:31:22 +0900

    chore: 사용하지 않는 scratch 파일 정리

--- scratch/test_naver_api.ts
+++
@@ -1,9 +0,0 @@
-
-async function testFetch() {
-  const url = 'https://m.stock.naver.com/api/stocks/marketValue/KOSPI?page=1&pageSize=5'
-  const res = await fetch(url)
-  const data = await res.json()
-  console.log('First stock data:')
-  console.log(JSON.stringify(data.stocks[0], null, 2))
-}
-testFetch()
